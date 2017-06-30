6d76e9f9cacda8743e5b5aff7a341ca17af570a1
Sergio Peluzzi <sergiopeluzzi@gmail.com>
2017-06-30 17:34:52 -0400

    Enviando arquivos

--- src/app/restaurants/restaurants.service.ts
+++ src/app/restaurants/restaurants.service.ts
@@ -3,9 +3,12 @@ import { Http } from '@angular/http'
 
 import { Observable } from 'rxjs/Observable'
 import 'rxjs/add/operator/map'
+import 'rxjs/add/operator/catch'
 
 import { Restaurant } from './restaurant/restaurant.model'
+import { ErrorHandler } from '../app.error-handler'
 import { USE_API } from '../app.api'
+
 
 @Injectable()
 export class RestaurantsService {
@@ -13,6 +16,8 @@ export class RestaurantsService {
     constructor(private http: Http){}
 
     restaurants(): Observable<Restaurant[]> {
-        return this.http.get(`${USE_API}/restaurants`).map(response => response.json());
+        return this.http.get(`${USE_API}/restaurants`)
+                .map(response => response.json())
+                .catch(ErrorHandler.handleError);
     }
 }
